1abcf337ef1f772a18c5f57b7323d9c3f9d8e7e3
aveliz1999 <github@veliz99.com>
2020-05-19 08:55:59 -0400

    Update table tests

--- src/components/table/Table.spec.tsx
+++ src/components/table/Table.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {mount} from 'enzyme';
 
 import Table from "./Table";
+import axios from "axios";
 
 describe('Table', () => {
     it('Renders the headers', () => {
@@ -15,7 +16,11 @@ describe('Table', () => {
 
     it('Renders the data', () => {
         const headersData = [{key: 'first', display: 'Header 1'}, {key: 'second', display: 'Header 2'}];
-        const data = [{id: 0, first: '12345', second: "67890"}, {id: 1, first: '34567', second: '89012'}, {id: 2, first: '56789', second: '01234'}];
+        const data = [{id: 0, first: '12345', second: "67890"}, {id: 1, first: '34567', second: '89012'}, {
+            id: 2,
+            first: '56789',
+            second: '01234'
+        }];
         const component = mount(<Table data={data} headers={headersData}/>);
 
         component.find('.dataRow').forEach((row, rowIndex) => {
@@ -26,13 +31,19 @@ describe('Table', () => {
 
     it('Filters the data correctly', () => {
         const headersData = [{key: 'first', display: 'Header 1'}, {key: 'second', display: 'Header 2'}];
-        const data = [{id: 0, first: '12345', second: "67890"}, {id: 1, first: '34567', second: '89012'}, {id: 2, first: '56789', second: '01234'}];
-        const component = mount(<Table data={data} headers={headersData}/>);
+        const data = [{id: 0, first: '12345', second: "67890"}, {id: 1, first: '34567', second: '89012'}, {
+            id: 2,
+            first: '56789',
+            second: '01234'
+        }];
 
-        for(let dataEntry of data) {
-            component.find('.filterInput').at(0).simulate('change', { target: { value: dataEntry.first } });
-            expect(component.find('.dataRow').at(0).childAt(0).text()).toBe(dataEntry.first);
-            expect(component.find('.dataRow').at(0).childAt(1).text()).toBe(dataEntry.second);
+        const updateFunction = jest.fn((key: string, filter: string) => {
+        });
+        const component = mount(<Table data={data} headers={headersData} update={updateFunction}/>);
+
+        for (let dataEntry of data) {
+            component.find('.filterInput').at(0).simulate('change', {target: {value: dataEntry.first}});
+            expect(updateFunction).toHaveBeenLastCalledWith('first', dataEntry.first);
         }
     });
 });
