add63450ef12c6b7d813e90b404db5f23db3d52e
aveliz1999 <github@veliz99.com>
2020-08-14 19:28:26 -0400

    Add scanner path

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Login from "./components/login/Login";
 import {Redirect, Route, Switch} from "react-router-dom";
 import Home from "./components/home/Home";
+import Scanner from './components/scanner/Scanner';
 
 function App() {
     return <Switch>
         <Route exact path='/login' component={Login}/>
         <Route exact path='/home' component={Home}/>
+        <Route exact path='/scan' component={Scanner}/>
         <Route path='/'>
             <Redirect to='/login'/>
         </Route>
---
+++ src/components/scanner/Scanner.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BrowserBarcodeReader} from '@zxing/library';
+
+export type ScannerState = {
+    devices: MediaDeviceInfo[]
+}
+
+class Scanner extends React.Component<any, ScannerState> {
+    state: ScannerState = {
+        devices: []
+    }
+
+    codeReader: BrowserBarcodeReader;
+
+    constructor(props: any) {
+        super(props);
+        this.codeReader = new BrowserBarcodeReader();
+    }
+
+    async componentDidMount() {
+        const devices = (await this.codeReader.listVideoInputDevices()).filter(device => device.kind === 'videoinput');
+        this.setState({
+            devices
+        });
+
+        this.codeReader.decodeFromVideoDevice(devices[0].deviceId, 'video', (result, error) => {
+            if (result) {
+                alert(result.getText());
+            }
+        });
+    }
+
+    render(): React.ReactNode {
+        return <video id='video' style={{width: '100%', height: '100%'}}/>;
+    }
+}
+
+export default Scanner;
